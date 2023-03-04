bc7d2796f9d186a331343ac62858fbe8dd932e4a
Mike Slattery <michael.slattery@yale.edu>
2023-03-04 17:14:22 -0500

    started form-based config

---
+++ src/Form/YseResourcePullForm.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @file
+ * Simple config form.
+ */
+
+namespace Drupal\yse_resource_pull\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\Display\EntityFormDisplayInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class YseResourcePullForm extends ConfigFormBase {
+
+  /**
+   * Constructs an YseResourcePullForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    parent::__construct($config_factory);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'yse_resource_pull';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['yse_resource_pull.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   *  Should show a note here and link about role mapping over at CAS Attributes for bulk form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    
+    $resource_host = $this->configFactory->get('yse_resource_pull.settings')->get('resource_host');
+    $resource_apikey = $this->configFactory->get('yse_resource_pull.settings')->get('resource_apikey');
+
+    $form['resource_host'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Resource URL for lookups'),
+      '#default_value' => $resource_host,
+      '#min' => 1,
+      '#required' => TRUE,
+      '#description' => $this->t('The Resources URL for YSE data lookups.'),
+    ];
+
+    $form['resource_apikey'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#default_value' => $resource_apikey,
+      '#min' => 1,
+      '#required' => TRUE,
+      '#description' => $this->t('String that Resources needs for access.'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->configFactory->getEditable('yse_resource_pull.settings');
+    $config->set('resource_host', $form_state->getValue('resource_host'))->save();
+    $config->set('resource_apikey', $form_state->getValue('resource_apikey'))->save();
+    //\Drupal::logger('yse_cas_event_subscribers')->notice('Are we being called @yeah?', ['@yeah' => $yeah]);
+    parent::submitForm($form, $form_state);
+  }
+
+
+}
--- src/Twig/Extension/Functions.php
+++ src/Twig/Extension/Functions.php
@@ -2,8 +2,13 @@
 
 namespace Drupal\yse_resource_pull\Twig\Extension;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ConnectException;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+
 
 /**
  * Functions provides functions.
@@ -39,28 +44,45 @@ class Functions extends AbstractExtension {
    */
   public function getYseResource(string $type, string $key) {
 
-    $yse_resources_url = 'https://NOPE/';
-    $yse_resource_types = [
-      'courses' => '/YUP/',
-    ];
-    $yse_resource_mimes = [
-      'courses' => 'text/html',
-    ];
-
     if ($type && $key) {
-      $req = $yse_resources_url . $key;
-      $res = \Drupal::httpClient()->get($req, [
-        'headers' => [
-          'Content-Type' => 'application/x-www-form-urlencoded',
-          'Accept' => $yse_resource_mimes[$type],
-        ],
-      ]);
-      if ($res->getStatusCode() == 200){
-        $data = $res->getBody()->getContents();
-        return $data;
-      }
-      else {
-        return '<br />';
+      
+      $config = \Drupal::config('yse_resource_pull');
+      $resource_host   = $config->get('resource_host');
+      $resource_path   = $config->get('resource_paths.' . $type);
+      $resource_mime   = $config->get('resource_mimes.' . $type);
+      $resource_apikey = $config->get('resource_apikey');
+
+      if ((!empty($resource_host)) && (!empty($resource_path))){
+
+        $yse_resources_url = 'https://' . $resource_host . $resource_path . $key;
+
+        if (!empty($resource_apikey)){
+          $yse_resources_url .= '?' . $resource_apikey;
+        }
+
+        $guzzle = new Client(
+          ['headers' => [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Accept' => $resource_mime
+          ]
+        ]);
+
+        $req = $yse_resources_url;
+
+        try {
+          $res = $guzzle->request('GET', $req);
+          $data = $res->getBody()->getContents();
+          return $data;
+        }
+        catch (ConnectException $e) {
+          return '<-- Courses 4xx -->';
+        }
+        catch (RequestException $e) {
+          return '<-- Courses 4xx -->';
+        }
+        catch (ClientException $e) {
+          return '<-- Courses 4xx -->';
+        }
       }
     }
     return '';
